0eaad06d5784fdcbc9b28e3410da5bfb9896e23d
rene-paredes <a.rene.paredes@gmail.com>
2015-07-19 14:23:41 +0200

    Vistas parciales y marco

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //Comentamos la carga de la pagina users. Paso 2 modulo6
@@ -14,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//Instalamos mw partials generandolo con ()
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
