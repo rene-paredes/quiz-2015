c63965a884186eddd61f426469c8ef6372a0f501
rene-paredes <a.rene.paredes@gmail.com>
2015-07-20 15:27:31 +0200

    Añadir author en rama creditos

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-//enrutamos las peticiones GET de quiestion y answer 
+//GET author
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Quiz' });
+});
+
+//enrutamos las peticiones GET de question y answer 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
