90198fa4c37f454f82bb07e521ff1a3493fbf88c
Rene Paredes <a.rene.paredes@gmail.com>
2015-07-31 20:49:42 +0200

    modulo8 tema1-Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,3 +43,31 @@ exports.answer = function (req, res) {
 
 };
 
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build(	//crea objeto quiz
+	 				 {
+					  pregunta:"Pregunta" ,
+					  respuesta:"Respuesta"
+					 }
+				);
+	res.render('quizes/new', { quiz: quiz });
+};
+
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	//crear objeto quiz a  partir parametros body del formulario de quizes/new
+	var quiz = models.Quiz.build(  req.body.quiz);
+
+	//guardar en BD los campos pregunta y respuesta de quiz. 
+	//especificamos los campos para evitar inyeccion de codigo
+	quiz.save( { fields: ["pregunta", "respuesta"] })
+	    .then(function() {
+			//una vez creada pregunta redirigimos a /quizes
+			res.redirect('/quizes');
+		} )	
+
+};
+
+
