9bab55ac9c614e1a48411cc8ff801854dc8e0551
Rene Paredes <a.rene.paredes@gmail.com>
2015-08-20 13:40:55 +0200

    Modulo7: Ejercicio P2P busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,11 +18,28 @@ exports.load = function (req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function (req, res) {
-       models.Quiz.findAll()
-		  .then(function(quizes){
-		res.render('quizes/index.ejs', { quizes: quizes  });
-	}).catch(function(error) {next(error);})
+	var filtrar = ('%' + req.query.search + '%').replace(/\s/g,'%');
+	console.log('Filtro:'+ filtrar);
 
+	if (req.query.search) {
+	        models.Quiz.findAll(
+				    {
+			where: ["pregunta like ?", filtrar],
+			order: [['pregunta' , 'ASC' ]] 	
+				    }
+				)
+			  .then(function(quizes){
+			res.render('quizes/index.ejs', { quizes: quizes  });
+		}).catch(function(error) {next(error);})
+	}
+	else {
+
+	        models.Quiz.findAll()
+			  .then(function(quizes){
+			res.render('quizes/index.ejs', { quizes: quizes  });
+		}).catch(function(error) {next(error);})
+	
+	}
 };
 
 // GET /quizes/:quizId
